c7c716c4b5dd13354855fd7cce910bafbd05bb52
Hugo Flores <hfp.ing2@gmail.com>
2020-04-17 17:03:15 -0400

    School table cell

--- 20200416-HugoFlores-NYCSchools/Controllers/Cells/SchoolTableCell.h
+++ 20200416-HugoFlores-NYCSchools/Controllers/Cells/SchoolTableCell.h
@@ -7,11 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "School.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface SchoolTableCell : UITableViewCell
-
+@property UILabel *schoolNameLabel;
+@property UILabel *descriptionLabel;
+@property UILabel *stateLabel;
+@property UILabel *cityLabel;
+@property UILabel *addressLabel;
+@property School *model;
+- (void)setView;
 @end
 
 NS_ASSUME_NONNULL_END
--- 20200416-HugoFlores-NYCSchools/Controllers/Cells/SchoolTableCell.m
+++ 20200416-HugoFlores-NYCSchools/Controllers/Cells/SchoolTableCell.m
@@ -10,15 +10,58 @@
 
 @implementation SchoolTableCell
 
-- (void)awakeFromNib {
-    [super awakeFromNib];
-    // Initialization code
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    [self initObjects];
+    return self;
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
+}
 
-    // Configure the view for the selected state
+- (void)setUpView {
+    UIStackView *addresView = [[UIStackView alloc] initWithArrangedSubviews:@[_stateLabel, _cityLabel, _addressLabel]];
+    addresView.axis = UILayoutConstraintAxisHorizontal;
+    addresView.spacing = 6;
+    addresView.alignment = UIStackViewDistributionEqualSpacing;
+
+    UIStackView *mainView = [[UIStackView alloc] initWithArrangedSubviews:@[_schoolNameLabel, _descriptionLabel, addresView]];
+    mainView.translatesAutoresizingMaskIntoConstraints = false;
+    mainView.axis = UILayoutConstraintAxisVertical;
+    mainView.spacing = 8;
+    [mainView setLayoutMarginsRelativeArrangement:true];
+    mainView.layoutMargins = UIEdgeInsetsMake(12, 12, 12, 12);
+    
+    [self.contentView addSubview:mainView];
+    
+    [[mainView.topAnchor constraintEqualToAnchor: self.contentView.topAnchor] setActive:true];
+    [[mainView.bottomAnchor constraintEqualToAnchor: self.contentView.bottomAnchor] setActive:true];
+    [[mainView.leftAnchor constraintEqualToAnchor: self.contentView.leftAnchor] setActive:true];
+    [[mainView.rightAnchor constraintEqualToAnchor: self.contentView.rightAnchor] setActive:true];
+}
+
+- (void) initObjects {
+    _schoolNameLabel =  [[UILabel alloc] init];
+    [_schoolNameLabel setFont:[UIFont fontWithName:@"AcademyEngravedLetPlain" size:30]];
+    _schoolNameLabel.numberOfLines = 0;
+
+    _descriptionLabel = [[UILabel alloc] init];
+    [_descriptionLabel setFont:[UIFont fontWithName:@"AmericanTypewriter" size:15]];
+    _descriptionLabel.numberOfLines = 0;
+
+    _stateLabel = [[UILabel alloc] init];
+    _cityLabel = [[UILabel alloc] init];
+    _addressLabel = [[UILabel alloc] init];
+    [self setUpView];
+}
+
+- (void)setView {
+    _schoolNameLabel.text = _model.schoolName;
+    _descriptionLabel.text = _model.academicopportunities1;
+    _stateLabel.text = _model.stateCode;
+    _cityLabel.text = _model.city;
+    _addressLabel.text = _model.primaryAddressLine1;
 }
 
 @end
