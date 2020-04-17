e9814a7e5db5bf33863efc1f8acd942c8c3b7717
Hugo Flores <hfp.ing2@gmail.com>
2020-04-17 12:25:45 -0400

    Schools table cell component

---
+++ 20200416-HugoFlores-NYCSchools/Controllers/Cells/SchoolTableCell.h
@@ -0,0 +1,17 @@
+//
+//  SchoolTableCell.h
+//  20200416-HugoFlores-NYCSchools
+//
+//  Created by Hugo Flores Perez on 4/17/20.
+//  Copyright © 2020 Hugo Flores Perez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SchoolTableCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 20200416-HugoFlores-NYCSchools/Controllers/Cells/SchoolTableCell.m
@@ -0,0 +1,24 @@
+//
+//  SchoolTableCell.m
+//  20200416-HugoFlores-NYCSchools
+//
+//  Created by Hugo Flores Perez on 4/17/20.
+//  Copyright © 2020 Hugo Flores Perez. All rights reserved.
+//
+
+#import "SchoolTableCell.h"
+
+@implementation SchoolTableCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
